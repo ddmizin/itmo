d06f2efc0fa5b22a88571f5e7e449911b4721ed9
ddmizin <ddmizin@mail.ru>
2025-10-08 14:54:03 +0300

    08.10.25

--- test.cpp
+++ test.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <fstream>
 
-bool CompareStringPrefix(const char* str, const char* prefix) {
+struct Data {
+    char DataKey[100];
+    char DataValue[100];
+};
+
+bool bCompareStringPrefix(const char* str, const char* prefix) {
     int i = 0;
     while (prefix[i] != '\0') {
         if (str[i] != prefix[i]) {
@@ -12,68 +17,147 @@ bool CompareStringPrefix(const char* str, const char* prefix) {
     return true;
 }
 
-struct Files {
-    char* DataKeys[100000];
-    char* DataValues[100000];
-};
+bool bCompareString(const char* str1, const char* str2) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
 
-int main(int argc, char* argv[]){
-    std::ifstream TemplateFile;
-    std::ifstream DataFile;
-    char* TemplatePath;
-    char* DataPath;
-    char* OutputPath;
+void CopyString(char* new_string, const char* source) {
+    int i = 0;
+    while (source[i] != '\0') {
+        new_string[i] = source[i];
+        i++;
+    }
+    new_string[i] = '\0';
+}
+
+int LengthString(const char* str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
+bool bIsSpace(char c) {
+    return c == ' ' || c == '\t'  || c == '\n';
+}
+
+bool bIsValidSymbol(char c) {
+    return (c >= 'a' && c <= 'z') || 
+           (c >= 'A' && c <= 'Z') || 
+           (c >= '0' && c <= '9') || 
+           (c == '_');
+}
+
+void DeleteSpaces(char* str) {
+    if (str == nullptr) {
+        return;
+    }
+
+    int l = LengthString(str);
+    int start = 0;
+    int end = l - 1;
+
+    while (start < l && bIsSpace(str[start])) {
+        start++;
+    }
+
+    while (end >= 0 && bIsSpace(str[end])) {
+        end--;
+    }
+
+    int new_l = end - start + 1;
+    if (new_l > 0) {
+        for (int i = 0; i < new_l; ++i) {
+            str[i] = str[start + i];
+        }
+    }
+    str[new_l] = '\0';
+}
+
+const char* FindChar(const char* str, char c) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] == c) {
+            return str + i;
+        }
+        ++i;
+    }
+    return nullptr;
+}
+
+bool CommandLine(int argc, char* argv[], char*& TemplatePath, char*& DataPath, char*& OutputPath){
     bool bTemplate = false;
     bool bData = false;
     for (int i = 1; i < argc; ++i){
-        if (CompareStringPrefix(argv[i], "-t")){
+        if (bCompareString(argv[i], "-t")){
             if (i + 1 < argc){
                 TemplatePath = argv[++i];
                 bTemplate = true;
             }
             else {
-                return 2;
+                return false;
             }
         }
-        else if (CompareStringPrefix(argv[i], "-d")){
+        else if (bCompareString(argv[i], "-d")){
             if (i + 1 < argc){
                 DataPath = argv[++i];
                 bData = true;
             }
             else {
-                return 2;
+                return false;
             }
         }
-        else if (CompareStringPrefix(argv[i], "-o")){
+        else if (bCompareString(argv[i], "-o")){
             if (i + 1 < argc){
                 OutputPath = argv[++i];
             }
             else {
-                return 2;
+                return false;
             }
         }
-        else if (CompareStringPrefix(argv[i], "--template=")){
+        else if (bCompareStringPrefix(argv[i], "--template=")){
             TemplatePath = argv[i] + 11;
             bTemplate = true;
         }
-        else if (CompareStringPrefix(argv[i], "--data=")){
+        else if (bCompareStringPrefix(argv[i], "--data=")){
             DataPath = argv[i] + 7;
             bData = true;
         }
-        else if (CompareStringPrefix(argv[i], "--output=")){
+        else if (bCompareStringPrefix(argv[i], "--output=")){
             OutputPath = argv[i] + 9;
         }
         else {
-            return 2;
+            return false;
         }
     }
-    std::cout << "Template: " << TemplatePath << '\n';
-    std::cout << "Data: " << DataPath << '\n';
-    std::cout << "Output: " << OutputPath << '\n';
     if (!bTemplate || !bData){
-        std::cout << "2";
+        return false;
+    }
+    return TemplatePath != nullptr && DataPath != nullptr;
+}
+
+int ReadData(const char* DataPath, Data* Data, int& data_count){
+        std::ifstream DataFile(DataPath);
+        if (!DataFile.is_open()){
+            return 3;
+        }
+        char line[4096];
+        data_count = 0;
+}
+
+int main(int argc, char* argv[]){
+    char* TemplatePath = nullptr;
+    char* DataPath = nullptr;
+    char* OutputPath = nullptr;
+    if (!CommandLine(argc, argv, TemplatePath, DataPath, OutputPath)){
         return 2;
     }
-    DataFile.open(DataPath);
-    
 }
